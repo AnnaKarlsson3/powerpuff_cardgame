bb7a6f72a669caea4cfa817d7b1bde68f3f1dd22
Sofia <sofia.fredmans@gmail.com>
2020-11-11 12:45:55 +0100

    end game
    method  to end game and test added

--- src/com/powerpuff/cardgame/test/TestGame.java
+++ src/com/powerpuff/cardgame/test/TestGame.java
@@ -3,6 +3,7 @@ package com.powerpuff.cardgame.test;
 import com.powerpuff.cardgame.cardGame.Game;
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertFalse;
 import static junit.framework.TestCase.assertTrue;
 
 public class TestGame {
@@ -11,6 +12,12 @@ public class TestGame {
     public void testStartGame(){
         Game game = new Game();
         assertTrue(!game.gameOver);
+    }
+
+    @Test
+    public void testEndGame(){
+        Game game = new Game();
+        assertFalse(game.gameOver);
     }
 
 }
