37332a158f4772c481d83c395be533e41aff6b35
anna <zoora1234@hotmail.com>
2020-11-11 13:37:05 +0100

    removed code we dont need

--- src/com/powerpuff/cardgame/Main.java
+++ src/com/powerpuff/cardgame/Main.java
@@ -10,10 +10,6 @@ public class Main {
     public static void main(String[] args) {
         new Game().run();
 
-        PlayerDeck deck = new PlayerDeck();
-        System.out.println(deck.getPlayerCards());
-
-
 
 
     }
--- src/com/powerpuff/cardgame/cardGame/Game.java
+++ src/com/powerpuff/cardgame/cardGame/Game.java
@@ -30,8 +30,6 @@ public class Game {
             gameOver(hp, playerDeckList);
         }
 
-
-
         }
 
 
