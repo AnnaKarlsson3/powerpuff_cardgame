75cf1c68e0094c7910f6629b34ad556fc405ebea
anna <zoora1234@hotmail.com>
2020-11-11 13:12:56 +0100

    fixed some files and imports

--- src/com/powerpuff/cardgame/test/TestDisplay.java
+++ src/com/powerpuff/cardgame/test/TestDisplay.java
@@ -1,6 +1,7 @@
 package com.powerpuff.cardgame.test;
 
-import org.junit.Test;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestDisplay {
 
--- src/com/powerpuff/cardgame/test/TestPlayer.java
+++ src/com/powerpuff/cardgame/test/TestPlayer.java
@@ -1,4 +1,7 @@
 package com.powerpuff.cardgame.test;
 
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TestPlayer {
 }
