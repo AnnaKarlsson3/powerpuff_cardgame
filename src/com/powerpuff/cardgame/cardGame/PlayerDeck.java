94f8ff628ebc25e1334ddf57c0ed9bec34a06f5d
alebal14 <alexandra.b.balogh@gmail.com>
2020-11-12 12:30:37 +0100

    remove from commondeck

--- src/com/powerpuff/cardgame/cardGame/PlayerDeck.java
+++ src/com/powerpuff/cardgame/cardGame/PlayerDeck.java
@@ -26,7 +26,9 @@ public class PlayerDeck {
     }
 
     public List<Card> removeFromCommonDeck(){
-        return null;
+        commonDeck.removeAll(playerCards);
+        cardGenerator.setCommonDeck(commonDeck);
+        return commonDeck;
     }
 
 
