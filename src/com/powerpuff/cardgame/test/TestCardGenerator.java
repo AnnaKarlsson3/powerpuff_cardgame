6b9d40a17a64608bce5ed9b058f7601845425bb5
Sofia <sofia.fredmans@gmail.com>
2020-11-12 11:28:03 +0100

    test fail

--- src/com/powerpuff/cardgame/test/TestCardGenerator.java
+++ src/com/powerpuff/cardgame/test/TestCardGenerator.java
@@ -1,7 +1,11 @@
 package com.powerpuff.cardgame.test;
 
+import com.powerpuff.cardgame.cardGame.Card;
 import com.powerpuff.cardgame.cardGame.CardGenerator;
 import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class TestCardGenerator {
@@ -26,7 +30,17 @@ public class TestCardGenerator {
 
     @Test
         public void testShuffleMethod(){
-        Assertions.assertTrue(cardGenerator.cards != cardGenerator.shuffledCards);
+        Card card1 = new Card("fighter", "banana", 5);
+        Card card2 = new Card("fighter", "apple", 8);
+        Card card3 = new Card("fighter","pear", 10 );
+
+        List <Card> cardList = new ArrayList <Card>();
+        cardList.add(card1);
+        cardList.add(card2);
+        cardList.add(card3);
+
+        Assertions.assertEquals(cardList, cardGenerator.shuffleCards(cardList));
+
 
         }
 
