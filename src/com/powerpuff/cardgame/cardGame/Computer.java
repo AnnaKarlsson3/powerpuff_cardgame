e11fe89cfc1092e472c141581bcaf511ea0d4917
alebal14 <alexandra.b.balogh@gmail.com>
2020-11-16 10:16:45 +0100

    Revert "Alexandra and Snehal"
    This reverts commit 00a362db1ca5849b661a8a75e19fb93316f85355.

--- src/com/powerpuff/cardgame/cardGame/Computer.java
+++ src/com/powerpuff/cardgame/cardGame/Computer.java
@@ -17,46 +17,7 @@ public class Computer extends Player{
     }
 
     public Card playCard() {
-        int currentHp = getHp();
-        ArrayList<Card> cardsInHand = hand.cardsInHand;
-
-        if (!cardsInHand.isEmpty()) {
-            if (currentHp < 10) {
-                try {
-                    Card maxAction = cardsInHand.stream()
-                            .filter(c -> c.getType().startsWith("Action"))
-                            .max(Comparator.comparing(Card::getPoint))
-                            .orElseThrow(NoSuchElementException::new);
-                    System.out.println(maxAction);
-                    return maxAction;
-                } catch (NoSuchElementException e) {
-                    Card fighter = cardsInHand.stream()
-                            .filter(c -> c.getType().startsWith("Fighter"))
-                            .max(Comparator.comparing(Card::getPoint))
-                            .orElseThrow(NoSuchElementException::new);
-                    System.out.println(fighter);
-                    return fighter;
-                }
-            } else {
-                try {
-                    Card maxFighter = cardsInHand.stream()
-                            .filter(c -> c.getType().startsWith("Fighter"))
-                            .max(Comparator.comparing(Card::getPoint))
-                            .orElseThrow(NoSuchElementException::new);
-                    System.out.println(maxFighter);
-                    return maxFighter;
-                } catch (NoSuchElementException e) {
-                    Card action = cardsInHand.stream()
-                            .filter(c -> c.getType().startsWith("Action"))
-                            .max(Comparator.comparing(Card::getPoint))
-                            .orElseThrow(NoSuchElementException::new);
-                    System.out.println(action);
-                    return action;
-                }
-            }
-        } else {
-            return null;
-        }
+        return null;
     }
 
 
