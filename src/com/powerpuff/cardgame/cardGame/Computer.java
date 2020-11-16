7aed775b409f3d5d06c59f230642c5cf54d45213
alebal14 <alexandra.b.balogh@gmail.com>
2020-11-16 10:23:30 +0100

    playcard method
    par programming: snehal and alexandra

--- src/com/powerpuff/cardgame/cardGame/Computer.java
+++ src/com/powerpuff/cardgame/cardGame/Computer.java
@@ -1,6 +1,10 @@
 package com.powerpuff.cardgame.cardGame;
 
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.NoSuchElementException;
+
 public class Computer extends Player{
 
     public Computer() {
@@ -13,7 +17,46 @@ public class Computer extends Player{
     }
 
     public Card playCard(){
-        return null;
+        int currentHp = getHp();
+        ArrayList<Card> cardsInHand = hand.cardsInHand;
+
+        if (!cardsInHand.isEmpty()) {
+            if (currentHp < 10) {
+                try {
+                    Card maxAction = cardsInHand.stream()
+                            .filter(c -> c.getType().startsWith("Action"))
+                            .max(Comparator.comparing(Card::getPoint))
+                            .orElseThrow(NoSuchElementException::new);
+                    System.out.println(maxAction);
+                    return maxAction;
+                } catch (NoSuchElementException e) {
+                    Card fighter = cardsInHand.stream()
+                            .filter(c -> c.getType().startsWith("Fighter"))
+                            .max(Comparator.comparing(Card::getPoint))
+                            .orElseThrow(NoSuchElementException::new);
+                    System.out.println(fighter);
+                    return fighter;
+                }
+            } else {
+                try {
+                    Card maxFighter = cardsInHand.stream()
+                            .filter(c -> c.getType().startsWith("Fighter"))
+                            .max(Comparator.comparing(Card::getPoint))
+                            .orElseThrow(NoSuchElementException::new);
+                    System.out.println(maxFighter);
+                    return maxFighter;
+                } catch (NoSuchElementException e) {
+                    Card action = cardsInHand.stream()
+                            .filter(c -> c.getType().startsWith("Action"))
+                            .max(Comparator.comparing(Card::getPoint))
+                            .orElseThrow(NoSuchElementException::new);
+                    System.out.println(action);
+                    return action;
+                }
+            }
+        } else {
+            return null;
+        }
     }
 
 }
