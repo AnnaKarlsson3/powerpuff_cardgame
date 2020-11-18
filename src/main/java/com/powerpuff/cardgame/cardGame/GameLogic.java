7ffa10525bb67c92d7cf2a481165173bc4b46b50
anna <zoora1234@hotmail.com>
2020-11-18 11:48:59 +0100

    added instructions for playerTurn

--- src/main/java/com/powerpuff/cardgame/cardGame/GameLogic.java
+++ src/main/java/com/powerpuff/cardgame/cardGame/GameLogic.java
@@ -2,8 +2,14 @@ package com.powerpuff.cardgame.cardGame;
 
 public class GameLogic {
 
+
+
     public String checkCardType(Card playedCard){
         return playedCard.getType();
-
     }
+
+
+
+  
+
 }
