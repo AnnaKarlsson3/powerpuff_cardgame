1ee85dc98020c628a55f7c36ef32886933773f42
snehal patel <snehal.kathiriya@gmail.com>
2020-11-18 12:33:23 +0100

    wip isCardFighter()

--- src/main/java/com/powerpuff/cardgame/cardGame/GameLogic.java
+++ src/main/java/com/powerpuff/cardgame/cardGame/GameLogic.java
@@ -2,8 +2,23 @@ package com.powerpuff.cardgame.cardGame;
 
 public class GameLogic {
 
-    public String checkCardType(Card playedCard){
-        return playedCard.getType();
 
+    public void attack(Card attack, Card block) {
     }
+
+    Gameboard gameboard = new Gameboard();
+
+    public Boolean IsCardFighter(Card playedCard, Player player){
+        if(playedCard.getType().equals("Action")){
+            player.setHp(player.getHp() + playedCard.getPoint());
+            player.getHand().deletePlayedCard(playedCard);
+            player.getHand().addNewCardToHand();
+            return false;
+        }else{
+            gameboard.placePlayerCardOnGameboard(playedCard);
+            return true;
+        }
+    }
+
+
 }
