821bce8b1d69b53c056f5c1725075c6c7048e7f3
snehal patel <snehal.kathiriya@gmail.com>
2020-11-18 13:22:14 +0100

    test for checkCardType() test fails without method implementation

--- src/main/java/com/powerpuff/cardgame/cardGame/GameLogic.java
+++ src/main/java/com/powerpuff/cardgame/cardGame/GameLogic.java
@@ -1,23 +1,20 @@
 package com.powerpuff.cardgame.cardGame;
 
 public class GameLogic {
-    Gameboard gameboard = new Gameboard();
+    private Gameboard gameboard = new Gameboard();
 
 
     public void attack(Card attack, Card block) {
 
     }
 
-    public Boolean IsCardFighter(Card playedCard, Player player) {
-        if (playedCard.getType().equals("Action")) {
-            player.setHp(player.getHp() + playedCard.getPoint());
-            player.getHand().deletePlayedCard(playedCard);
-            player.getHand().addNewCardToHand();
-            return false;
-        } else {
-            gameboard.placePlayerCardOnGameboard(playedCard);
-            return true;
-        }
+    public Boolean isCardFighter(Card playedCard, Player player) {
+        return null;
+
+    }
+
+    public Gameboard getGameboard() {
+        return gameboard;
     }
 
 
--- src/test/java/TestGameLogic.java
+++ src/test/java/TestGameLogic.java
@@ -1,12 +1,18 @@
 
-import com.powerpuff.cardgame.cardGame.Card;
-import com.powerpuff.cardgame.cardGame.Computer;
-import com.powerpuff.cardgame.cardGame.Player;
+import com.powerpuff.cardgame.cardGame.*;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 
 public class TestGameLogic {
+    Game game;
+    GameLogic gameLogic;
+
+    @BeforeEach
+    public void init() {
+        game = new Game();
+        gameLogic = new GameLogic();
+    }
 
 
     @Test
@@ -20,8 +26,28 @@ public class TestGameLogic {
         assertTrue(damage > blockCard.getBlockPointPoint(), "damage is greater than blockpoints");
             computer.setHp(damage);
 
+    }
+
+    @Test
+    public void testIsCardFighter(){
+
+        Player player = game.player;
 
 
+        Card card1 = new Card("Action","actionCard",3,3);
+        assertFalse(gameLogic.isCardFighter(card1,player),"isCardFighter should be false");
+
+
+        int beforeBoardListSize = gameLogic.getGameboard().getPlayerActiveCards().size();
+        System.out.println("before" + beforeBoardListSize);
+
+        Card card2 = new Card("Fighter","fighterCard",3,3);
+        assertTrue(gameLogic.isCardFighter(card2,player),"isCardFighter should be true");
+
+        int afterBoardListSize = gameLogic.getGameboard().getPlayerActiveCards().size();
+        System.out.println("after" + afterBoardListSize);
+
+        assertNotEquals(beforeBoardListSize,afterBoardListSize);
     }
 
     
