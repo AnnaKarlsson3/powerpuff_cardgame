87de2533b5a48e92083a2edaccf15200ddd4778d
anna <zoora1234@hotmail.com>
2020-11-20 11:51:53 +0100

    Redoing block method and test. test4, test failed, arrays should be not equals

--- src/test/java/TestGameLogic.java
+++ src/test/java/TestGameLogic.java
@@ -47,27 +47,40 @@ public class TestGameLogic {
         Card blockCard = new Card("Fighter", "Dora", 1,1);
         Card attackCard2 = new Card("Fighter", "Kristi", 2, 2);
         Card blockCard2 = new Card("Fighter", "Berit", 5,5);
+        Card attackCard3 = new Card("Fighter", "Crush", 2, 2);
+        Card blockCard3 = new Card("Fighter", "crain", 2,2);
 
         ArrayList<Card> playerActiveCards = gameboard.playerActiveCards;
         playerActiveCards.add(attackCard);
         playerActiveCards.add(attackCard2);
+        playerActiveCards.add(attackCard3);
         ArrayList<Card> playerActiveCardsCopy = (ArrayList<Card>) playerActiveCards.clone();
 
         ArrayList<Card> computerActiveCards = gameboard.computerActiveCards;
         computerActiveCards.add(blockCard);
         computerActiveCards.add(blockCard2);
+        computerActiveCards.add(blockCard3);
         ArrayList<Card> computerActiveCardsCopy = (ArrayList<Card>) computerActiveCards.clone();
 
         assertArrayEquals(computerActiveCards.toArray(), computerActiveCardsCopy.toArray(), "Expected both to be equal");
-        assertEquals(1,  gameLogic.block(attackCard, blockCard, playerActiveCards, computerActiveCards), "hpDamage should be 1");
+        assertEquals(1,  gameLogic.block(attackCard, blockCard, playerActiveCards, computerActiveCards), "blockpoints should be 1");
         assertTrue(gameLogic.greater == attackCard.getPoint() > blockCard.getBlockPointPoint(), "attackpoints are greater than blockpoints");
         assertFalse(Arrays.equals(computerActiveCards.toArray(), computerActiveCardsCopy.toArray()), "Expected both not to be equal");
 
         //--------------------
 
         assertArrayEquals(playerActiveCards.toArray(), playerActiveCardsCopy.toArray(), "Expected both to be equal");
-        assertEquals(3,  gameLogic.block(attackCard2, blockCard2, playerActiveCards, computerActiveCards), "hpDamage should be 3");
+        assertEquals(3,  gameLogic.block(attackCard2, blockCard2, playerActiveCards, computerActiveCards), "blockpoints should be 3");
         assertTrue(gameLogic.less == attackCard2.getPoint() < blockCard2.getBlockPointPoint(), "attackpoints are less than blockpoints");
+        assertFalse(Arrays.equals(playerActiveCards.toArray(), playerActiveCardsCopy.toArray()), "Expected both not to be equal");
+
+        //--------------------
+        computerActiveCardsCopy.remove(blockCard);
+        playerActiveCardsCopy.remove(attackCard2);
+        assertArrayEquals(computerActiveCards.toArray(), computerActiveCardsCopy.toArray(), "Expected both to be equal");
+        assertArrayEquals(playerActiveCards.toArray(), playerActiveCardsCopy.toArray(), "Expected both to be equal");
+        assertEquals(2,  gameLogic.block(attackCard3, blockCard3, playerActiveCards, computerActiveCards), "blockpoints should be 2");
+        assertFalse(Arrays.equals(computerActiveCards.toArray(), computerActiveCardsCopy.toArray()), "Expected both not to be equal");
         assertFalse(Arrays.equals(playerActiveCards.toArray(), playerActiveCardsCopy.toArray()), "Expected both not to be equal");
     }
 
